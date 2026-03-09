68c1872227339adc9d2309b9cd93d359bc2e7be5
tribotechtri-beep <tribotechtri@gmail.com>
2026-03-09 13:37:49 -0300

    Adicionar página de produtos

---
+++ Links.md
@@ -0,0 +1,23 @@
+# Links:
+## Nutrição
+Dux Hydrolite - [https://mercadolivre.com/sec/24pHJg8](https://mercadolivre.com/sec/24pHJg8)
+Dux Energy Kick - [https://mercadolivre.com/sec/2Sz16Pi](https://mercadolivre.com/sec/2Sz16Pi)
+Gel Paceit - [https://mercadolivre.com/sec/2h4m38v](https://mercadolivre.com/sec/2h4m38v)
+Gel Xtratus - [https://mercadolivre.com/sec/2xW8pcL](https://mercadolivre.com/sec/2xW8pcL)
+
+## Equipamentos
+Garmin Fênix 8 - [https://mercadolivre.com/sec/1CSCUcw](https://mercadolivre.com/sec/1CSCUcw)
+Garmin Forerunner 970 - [https://mercadolivre.com/sec/1oCtZLt](https://mercadolivre.com/sec/1oCtZLt)
+Garmin Edge 850 - [https://mercadolivre.com/sec/27HSNku](https://mercadolivre.com/sec/27HSNku)
+Garmin Varia 515 - [https://mercadolivre.com/sec/2oC5utR](https://mercadolivre.com/sec/2oC5utR)
+Asics Novablast 5 - [https://mercadolivre.com/sec/1GxuHTp](https://mercadolivre.com/sec/1GxuHTp)
+
+## Magazinagem
+Oakley Meta Vanguard - [https://mercadolivre.com/sec/1MSWuCA](https://mercadolivre.com/sec/1MSWuCA)
+Insta360 Go3s - [https://mercadolivre.com/sec/2jeZUR8](https://mercadolivre.com/sec/2jeZUR8)
+Microfone Hollyland Lark M2 - [https://mercadolivre.com/sec/2JszyfY](https://mercadolivre.com/sec/2JszyfY) 
+
+## Acessórios
+Cadarço de elástico - [https://mercadolivre.com/sec/1sd3n7G](https://mercadolivre.com/sec/1sd3n7G)
+Porta número de corrida - [https://mercadolivre.com/sec/1evJobc](https://mercadolivre.com/sec/1evJobc)
+Cinto elástico porta objetos - [https://mercadolivre.com/sec/1FwkJRg](https://mercadolivre.com/sec/1FwkJRg)
---
+++ produtos-data.js
@@ -0,0 +1,91 @@
+// CONFIGURAÇÃO DOS PRODUTOS - EDITE APENAS AQUI!
+// Salve as imagens na mesma pasta e coloque os nomes corretos abaixo
+
+const produtos = {
+    nutricao: [
+        {
+            nome: "Dux Hydrolite",
+            imagem: "dux-hydrolite.jpg",  // ← Nome da imagem que você vai salvar
+            link: "https://mercadolivre.com/sec/24pHJg8"
+        },
+        {
+            nome: "Dux Energy Kick",
+            imagem: "dux-energy-kick.jpg",
+            link: "https://mercadolivre.com/sec/2Sz16Pi"
+        },
+        {
+            nome: "Gel Paceit",
+            imagem: "gel-paceit.jpg",
+            link: "https://mercadolivre.com/sec/2h4m38v"
+        },
+        {
+            nome: "Gel Xtratus",
+            imagem: "gel-xtratus.jpg",
+            link: "https://mercadolivre.com/sec/2xW8pcL"
+        }
+    ],
+    
+    equipamentos: [
+        {
+            nome: "Garmin Fênix 8",
+            imagem: "garmin-fenix-8.jpg",
+            link: "https://mercadolivre.com/sec/1CSCUcw"
+        },
+        {
+            nome: "Garmin Forerunner 970",
+            imagem: "garmin-forerunner-970.jpg",
+            link: "https://mercadolivre.com/sec/1oCtZLt"
+        },
+        {
+            nome: "Garmin Edge 850",
+            imagem: "garmin-edge-850.jpg",
+            link: "https://mercadolivre.com/sec/27HSNku"
+        },
+        {
+            nome: "Garmin Varia 515",
+            imagem: "garmin-varia-515.jpg",
+            link: "https://mercadolivre.com/sec/2oC5utR"
+        },
+        {
+            nome: "Asics Novablast 5",
+            imagem: "asics-novablast-5.jpg",
+            link: "https://mercadolivre.com/sec/1GxuHTp"
+        }
+    ],
+    
+    magazinagem: [
+        {
+            nome: "Oakley Meta Vanguard",
+            imagem: "oakley-meta-vanguard.jpg",
+            link: "https://mercadolivre.com/sec/1MSWuCA"
+        },
+        {
+            nome: "Insta360 Go3s",
+            imagem: "insta360-go3s.jpg",
+            link: "https://meli.la/2ioKYVR"
+        },
+        {
+            nome: "Microfone Hollyland Lark M2",
+            imagem: "hollyland-lark-m2.jpg",
+            link: "https://mercadolivre.com/sec/2JszyfY"
+        }
+    ],
+    
+    acessorios: [
+        {
+            nome: "Cadarço de elástico",
+            imagem: "cadarco-elastico.jpg",
+            link: "https://mercadolivre.com/sec/1sd3n7G"
+        },
+        {
+            nome: "Porta número de corrida",
+            imagem: "porta-numero.jpg",
+            link: "https://mercadolivre.com/sec/1evJobc"
+        },
+        {
+            nome: "Cinto elástico porta objetos",
+            imagem: "cinto-porta-objetos.jpg",
+            link: "https://mercadolivre.com/sec/1FwkJRg"
+        }
+    ]
+};
